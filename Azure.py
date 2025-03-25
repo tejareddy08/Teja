d28b80fa0c2a29c6a853a0f5f6d182231ff3e1a4
Kutty <161919863+meshadivya@users.noreply.github.com>
2025-03-25 22:56:08 +0530

    Update Azure.py

--- Azure.py
+++ Azure.py
@@ -20,18 +20,27 @@ How HDFS STORE HUGE AMAOUNTS OF DATA AND PROCESS HUGE AMOUNTS OF DATA?
 --slave node - worker node  -- executes actual task
 
 --cluster - group of nodes / group of instances
---instance - part of cluster
+--instance - part of cluster- worker node
 
 how data will store in hdfs?
 
 s1- client submits the job
 s2- master node/driver node seggregates the data and distributes among worker nodes
-s3- actual task will be executed in worler nodes
+s3- actual task will be executed in worker nodes
 
 --each worker node can hold 128 mb of data
 --for every 10secs worker nodes will send heartbeat signals to driver node of exceeds 10 secs driver nodes assumes worker node to be died and creates another worker node with the duplicates
 --each worker node have 3 duplicates also called as replication factor --- falut tolerance mechanism
 --default replication factor - 3
+
+--hadoop v1 - single point failure -- name node failure
+--hadoop v2 - overcomed usimg secondary name node concept
+
+--secondary name node comes into play whenever the primary name node fails
+
+
+
+
 
 
 
